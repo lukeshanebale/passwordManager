6c4d0ba615218164f5a75a4d2965911845c9d844
Luke Bale <lukeshanebale@gmail.com>
2019-09-22 16:23:35 +0100

    Addition of the Login Page
    This is the addition of the login page, this uses the Login Form 12 by Colorlib and i have created the controllers to match as well as a few other bits to ensure that this is the first thing anyone see's coming to this page.

--- passwordManager/Startup.cs
+++ passwordManager/Startup.cs
@@ -57,7 +57,7 @@ namespace passwordManager
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
